36f1acaaad82c9d76990111af44c57553f608fb2
Bheemesh <gbheemesh888@gmail.com>
2023-02-08 16:44:33 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+     //   r.name = 10;
+     //   System.out.println(r.name);
+        // name has private access in com.driver.RWOnly
+        r.setter(10);
+        r.getter();
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private int name;
+
+   public  void setter(int a)
+    {
+        name = a;
+    }
+
+   public  int getter()
+    {
+        return name;
+    }
+
 }
